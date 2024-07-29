0a7c2dd5236e13ebbdfe449cad54583192fa27ac
fox-tomoki-tabei <tomoki.tabei@fox-hound.jp>
2024-07-29 15:01:12 +0900

    first commit

---
+++ src/main/java/com/example/bestalbum/BestalbumApplication.java
@@ -0,0 +1,13 @@
+package com.example.bestalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestalbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestalbumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bestalbum/BestalbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bestalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestalbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
